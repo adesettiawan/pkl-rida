b8682f0280212b9f6e5d673a828000e88b46e0f2
Ade Setiawan <adesetiawan0675@gmail.com>
2023-05-22 23:35:57 +0700

    add function login

--- app/Views/_layouts/_partials/navbar.php
+++ app/Views/_layouts/_partials/navbar.php
@@ -21,18 +21,18 @@
     <ul class="navbar-nav navbar-right">
         <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user"> <img alt="image" src="assets/img/user.png" class="user-img-radious-style"> <span class="d-sm-none d-lg-inline-block"></span></a>
             <div class="dropdown-menu dropdown-menu-right pullDown">
-                <div class="dropdown-title">Hello Sarah Smith</div>
+                <div class="dropdown-title">Hello, <?= session()->get('name') ?></div>
                 <a href="profile.html" class="dropdown-item has-icon"> <i class="far
 										fa-user"></i> Profile
-                </a> <a href="timeline.html" class="dropdown-item has-icon"> <i class="fas fa-bolt"></i>
+                    <!-- </a> <a href="timeline.html" class="dropdown-item has-icon"> <i class="fas fa-bolt"></i>
                     Activities
                 </a> <a href="#" class="dropdown-item has-icon"> <i class="fas fa-cog"></i>
                     Settings
-                </a>
-                <div class="dropdown-divider"></div>
-                <a href="<?= base_url('logout') ?>" class="dropdown-item has-icon text-danger"> <i class="fas fa-sign-out-alt"></i>
-                    Logout
-                </a>
+                </a> -->
+                    <div class="dropdown-divider"></div>
+                    <a href="<?= base_url('logout') ?>" class="dropdown-item has-icon text-danger"> <i class="fas fa-sign-out-alt"></i>
+                        Logout
+                    </a>
             </div>
         </li>
     </ul>
