754e57012fba0510812e2c043f9ad6d8003b38f1
Ade Setiawan <adesetiawan0675@gmail.com>
2023-05-22 08:52:23 +0700

    update

--- app/Views/_layouts/_partials/footer.php
+++ app/Views/_layouts/_partials/footer.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="footer-left">
-        <a href="templateshub.net">Templateshub</a></a>
+        <a href="">PKL Bidang RIDA</a></a>
     </div>
     <div class="footer-right">
     </div>
